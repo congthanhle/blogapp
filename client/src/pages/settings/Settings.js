4500767b0491f8fc493a0e060b96a72ea5be8ca7
Pham thuc <phamtrithuc22022000@gmail.com>
2023-09-21 09:48:14 +0700

    update and change name of variable and function in setting file

--- client/src/pages/settings/Settings.js
+++ client/src/pages/settings/Settings.js
@@ -7,82 +7,81 @@ import { Context } from "../../context/Context";
 import axios from "axios";
 
 export default function Settings() {
-  const [file, setfile] = useState(null);
-  const [username, setusername] = useState("");
-  const [email, setemail] = useState("");
-  const [password, setpassword] = useState("");
-  const [success, setsuccess] = useState(false);
-
+  const [file, setFile] = useState(null);
+  const [username, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
   const { user, dispatch } = useContext(Context);
   const PF = "http://localhost:5000/images/";
 
-  const handlesubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     dispatch({ type: "UPDATE_START" });
-    const updateduser = {
+    const updateUser = {
       userId: user._id,
       username,
       email,
       password,
     };
     if (file) {
-      const dulieu = new FormData();
-      const tenfile = Date.now() + file.name;
-      dulieu.append("name", tenfile);
-      dulieu.append("file", file);
-      updateduser.profilePic = tenfile;
+      const data = new FormData();
+      const fileName = Date.now() + file.name;
+      data.append("name", fileName);
+      data.append("file", file);
+      updateUser.profilePic = fileName;
       try {
-        await axios.post("/upload", dulieu);
+        await axios.post("/upload", data);
       } catch (err) {}
     }
     try {
-      const res = await axios.put("/users/" + user._id, updateduser);
-      setsuccess(true);
-      dispatch({ type: "UPDATE_SUCCESS", payload: res.dulieu });
+      const res = await axios.put("/users/" + user._id, updateUser);
+      setSuccess(true);
+      dispatch({ type: "UPDATE_SUCCESS", payload: res.data });
     } catch (err) {
       dispatch({ type: "UPDATE_FAILURE" });
     }
   };
   return (
     <div className="settings">
-      <div className="settingswrapper">
-        <div className="settingstitle">
-          <span className="settingupdatetitle">Update Your Account</span>
-          <span className="settingsdeletetitle">Delete Account</span>
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingUpdateTitle">Update Your Account</span>
+          <span className="settingsDeleteTitle">Delete Account</span>
         </div>
-        <form className="settingsform" onSubmit={handlesubmit}>
+        <form className="settingsForm" onSubmit={handleSubmit}>
           <label>Profile Picture</label>
           <div className="settingsPP">
             <img
               src={file ? URL.createObjectURL(file) : PF + user.profilePic}
               alt=""
             />
-            <label htmlFor="filenput">
+            <label htmlFor="fileInput">
               <i className="settingsPPIcon far fa-user-circle"></i>
             </label>
             <input
               type="file"
               id="fileInput"
               style={{ display: "none" }}
-              onChange={(e) => setfile(e.target.files[0])}
+              onChange={(e) => setFile(e.target.files[0])}
             />
           </div>
           <label>Username</label>
           <input
             type="text"
             placeholder={user.username}
-            onChange={(e) => setusername(e.target.value)}
+            onChange={(e) => setUserName(e.target.value)}
           />
           <label>Email</label>
           <input
             type="email"
             placeholder={user.email}
-            onChange={(e) => setemail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <label>Password</label>
           <input
             type="password"
-            onChange={(e) => setpassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <button className="settingsSubmit" type="submit">
             Update
