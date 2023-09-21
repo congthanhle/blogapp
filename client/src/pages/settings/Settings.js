5a3a36aa8bc356d389a1452d228b8fc66709d754
Pham thuc <phamtrithuc22022000@gmail.com>
2023-09-21 09:01:59 +0700

    change code convention

--- client/src/pages/settings/Settings.js
+++ client/src/pages/settings/Settings.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import "./settings.css";
 import Sidebar from "../../components/sidebar/Sidebar";
 import { useContext, useState } from "react";
@@ -5,82 +7,84 @@ import { Context } from "../../context/Context";
 import axios from "axios";
 
 export default function Settings() {
-  const [file, setFile] = useState(null);
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [success, setSuccess] = useState(false);
+  const [file, setfile] = useState(null);
+  const [username, setusername] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [success, setsuccess] = useState(false);
 
   const { user, dispatch } = useContext(Context);
-  const PF = "http://localhost:5000/images/"
+  const PF = "http://localhost:5000/images/";
 
-  const handleSubmit = async (e) => {
+  const handlesubmit = async (e) => {
     e.preventDefault();
     dispatch({ type: "UPDATE_START" });
-    const updatedUser = {
+    const updateduser = {
       userId: user._id,
       username,
       email,
       password,
     };
+
+    //upload du lieu
     if (file) {
-      const data = new FormData();
-      const filename = Date.now() + file.name;
-      data.append("name", filename);
-      data.append("file", file);
-      updatedUser.profilePic = filename;
+      const dulieu = new FormData();
+      const tenfile = Date.now() + file.name;
+      dulieu.append("name", tenfile);
+      dulieu.append("file", file);
+      updateduser.profilePic = tenfile;
       try {
-        await axios.post("/upload", data);
+        await axios.post("/upload", dulieu);
       } catch (err) {}
     }
     try {
-      const res = await axios.put("/users/" + user._id, updatedUser);
-      setSuccess(true);
-      dispatch({ type: "UPDATE_SUCCESS", payload: res.data });
+      const res = await axios.put("/users/" + user._id, updateduser);
+      setsuccess(true);
+      dispatch({ type: "UPDATE_SUCCESS", payload: res.dulieu });
     } catch (err) {
       dispatch({ type: "UPDATE_FAILURE" });
     }
   };
   return (
     <div className="settings">
-      <div className="settingsWrapper">
-        <div className="settingsTitle">
-          <span className="settingsUpdateTitle">Update Your Account</span>
-          <span className="settingsDeleteTitle">Delete Account</span>
+      <div className="settingswrapper">
+        <div className="settingstitle">
+          <span className="settingupdatetitle">Update Your Account</span>
+          <span className="settingsdeletetitle">Delete Account</span>
         </div>
-        <form className="settingsForm" onSubmit={handleSubmit}>
+        <form className="settingsform" onSubmit={handlesubmit}>
           <label>Profile Picture</label>
           <div className="settingsPP">
             <img
-              src={file ? URL.createObjectURL(file) : PF+user.profilePic}
+              src={file ? URL.createObjectURL(file) : PF + user.profilePic}
               alt=""
             />
-            <label htmlFor="fileInput">
+            <label htmlFor="filenput">
               <i className="settingsPPIcon far fa-user-circle"></i>
             </label>
             <input
               type="file"
               id="fileInput"
               style={{ display: "none" }}
-              onChange={(e) => setFile(e.target.files[0])}
+              onChange={(e) => setfile(e.target.files[0])}
             />
           </div>
           <label>Username</label>
           <input
             type="text"
             placeholder={user.username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={(e) => setusername(e.target.value)}
           />
           <label>Email</label>
           <input
             type="email"
             placeholder={user.email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => setemail(e.target.value)}
           />
           <label>Password</label>
           <input
             type="password"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => setpassword(e.target.value)}
           />
           <button className="settingsSubmit" type="submit">
             Update
--- client/src/pages/write/Write.js
+++ client/src/pages/write/Write.js
@@ -1,60 +1,62 @@
+/** @format */
+
 import { useContext, useState } from "react";
 import "./write.css";
 import axios from "axios";
 import { Context } from "../../context/Context";
 
-const URL = 'http://localhost:5000';
+const URL = "http://localhost:5000";
 
 export default function Write() {
-  const [title, setTitle] = useState("");
-  const [desc, setDesc] = useState("");
-  const [file, setFile] = useState(null);
+  const [title, settitle] = useState("");
+  const [desc, setdesc] = useState("");
+  const [file, setfile] = useState(null);
   const { user } = useContext(Context);
 
-  const handleSubmit = async (e) => {
+  const handlesubmit = async (e) => {
     e.preventDefault();
-    const newPost = {
+    const baipostmoi = {
       username: user.username,
       title,
       desc,
     };
     if (file) {
-      const data =new FormData();
-      const filename = Date.now() + file.name;
-      data.append("name", filename);
-      data.append("file", file);
-      newPost.photo = filename;
+      const dulieu = new FormData();
+      const tenfile = Date.now() + file.name;
+      dulieu.append("name", tenfile);
+      dulieu.append("file", file);
+      baipostmoi.photo = tenfile;
       try {
-        await axios.post(`${URL}/upload`, data);
+        await axios.post(`${URL}/upload`, dulieu);
       } catch (err) {}
     }
     try {
-      const res = await axios.post(`${URL}/posts`, newPost);
-      window.location.replace('/post/' + res.data._id);
+      const res = await axios.post(`${URL}/posts`, baipostmoi);
+      window.location.replace("/post/" + res.dulieu._id);
     } catch (err) {}
   };
   return (
     <div className="write">
       {file && (
-        <img className="writeImg" src={URL.createObjectURL(file)} alt="" />
+        <img className="writeimg" src={URL.createObjectURL(file)} alt="" />
       )}
-      <form className="writeForm" onSubmit={handleSubmit}>
-        <div className="writeFormGroup">
-          <label htmlFor="fileInput">
+      <form className="writeform" onSubmit={handlesubmit}>
+        <div className="writeformgroup">
+          <label htmlFor="fileinput">
             <i className="writeIcon fas fa-plus"></i>
           </label>
           <input
             type="file"
             id="fileInput"
             style={{ display: "none" }}
-            onChange={(e) => setFile(e.target.files[0])}
+            onChange={(e) => setfile(e.target.files[0])}
           />
           <input
             type="text"
             placeholder="Title"
             className="writeInput"
             autoFocus={true}
-            onChange={e=>setTitle(e.target.value)}
+            onChange={(e) => settitle(e.target.value)}
           />
         </div>
         <div className="writeFormGroup">
@@ -62,7 +64,7 @@ export default function Write() {
             placeholder="Tell your story..."
             type="text"
             className="writeInput writeText"
-            onChange={e=>setDesc(e.target.value)}
+            onChange={(e) => setdesc(e.target.value)}
           ></textarea>
         </div>
         <button className="writeSubmit" type="submit">
